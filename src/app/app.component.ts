f1120d8a3fd793d7629f4393088ad771294ddc76
AmosCosy <1335199417@qq.com>
2017-09-24 22:02:55 +0800

    update
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,67 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /**
+   * Annotation2
+   */
+  /* template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero}} details!
+  ` */
+
+  /**
+   * Annotation5
+   */
+  /* template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+  ` */
+
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero}}</h2>
+  <p>Heroes:</p>
+  <ul>
+    <li *ngFor="let hero of heroes">
+      {{ hero }}
+    </li>
+  </ul>
+`
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  /**
+   * Annotation1
+   */
+  //title = 'My First Angular App';
+
+  /**
+   * Annotation2
+   */
+  /* title = 'Tour of Heros';
+  hero = 'WindStorm'; */
+
+  /**
+   * Annotation3
+   */
+  /* title = 'Tour of Heroes';
+  myHero = 'Windstorm'; */
+
+  /**
+   * Annotation4
+   * 
+   * @type {string}
+   * @memberof AppComponent
+   */
+  /* title: string;
+  myHero: string;
+
+  constructor() {
+    this.title = 'Tour of Heroes';
+    this.myHero = 'Windstorm';
+  } */
+
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
 }
